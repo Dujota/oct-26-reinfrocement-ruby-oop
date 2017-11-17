751dfd360bfe52888962041e3801bc0db00dd3fe
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-11-17 01:10:27 -0500

    created if statement to capture all four conditions, word length > 4 & downcase, word>4 & downcase else print word

--- ex1.rb
+++ ex1.rb
@@ -1,1 +1,15 @@
 array = ["Totam", "ut", "odit","quis", "Maiores", "unde", "EX", "EST", "corporis"]
+
+array.each do |word|
+
+  if word.length > 4 && word == word.downcase
+    puts "long and downcase"
+  elsif word.length > 4
+    puts "long"
+  elsif word == word.downcase
+    puts "lowercase"
+  else
+   puts word
+  end
+
+end
